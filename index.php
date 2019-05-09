0d3365be41d335140d4ee3fd348deb1796fbb87e
Qu <Quanvle@live.dk>
2019-05-09 15:04:56 +0200

    Operatore i php
    https://www.nemprogrammering.dk/Tutorials/PHP/PHP/7-operatorer.php

--- index.php
+++ index.php
@@ -1,33 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="nemprogrammering" content="php">
-    <title>PHP</title>
-</head>
-<body>
-    <p>I am HTML</p> <?php echo"<h1>i am PHP</h1>"?>
+<?php
+    $var1 = 2;
+    $var2 = 3;
 
-    <?php
-        $age = 21;
-        echo "I am $age year old";
-        echo"<br>";
-    ?>
+    $result = $var1 + $var2;
 
-<!-- https://www.nemprogrammering.dk/Tutorials/PHP/PHP/6-datatyper.php -->
+    $var3 = 15;
+    $var4 = 5.5;
 
-    <?php
-        $dataInt = 31;
-        $dataDouble = 31.5;
-        $dataString = "december";
+    $result2 = $var3 / $var4;
 
-        echo  "<br>Jeg er en datatype int = $dataInt";
-        echo  "<br>Jeg er en datatype double = $dataDouble";
-        echo  "<br>Jeg er en datatype string = $dataString";
+    echo $result2;
 
-        echo "<br>brug i forbindelse med echo for at udprinte variablens værdi og single quotes '' for at udprinte variablens navn";
-
-    ?>
-
-</body>
-</html>
+?>
