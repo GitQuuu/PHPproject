d5b3068e0faa1701e8454a28fbcb54a17e9035a3
Qu <Quanvle@live.dk>
2019-05-09 13:59:29 +0200

    learning about variabel in php

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="nemprogrammering" content="php">
+    <title>PHP</title>
 </head>
 <body>
     <p>I am HTML</p> <?php echo"<h1>i am PHP</h1>"?>
+
+    <?php
+        $age = 21;
+        echo "I am $age year old";
+    ?>
 
 </body>
 </html>
