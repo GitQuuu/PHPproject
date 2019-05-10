fbf9f5f1c73debe2e619cfef86cfc7267847ec42
Qu <Quanvle@live.dk>
2019-05-10 11:35:52 +0200

    while loops
    https://www.nemprogrammering.dk/Tutorials/PHP/PHP/11-while-loops.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-    $Cube = 2;
-    //post increment
-    $Cube++;
+    // udskriv 10-tabellen med en while loop
+    $Tabel = 1;
+    while ( $Tabel >= 10){
+        echo $Tabel;
+        echo "<br>";
 
-    $cube = 3;
-    ++$cube;
+        $Tabel++;
+    }
 
-    echo "PostIncrement = ";
-    echo $Cube;
-    echo"<br>";
-    echo "PreIncrement = ";
-    echo $cube;
+// udskriv tallen 0 til 100 med en while loop
 
-
-?>
+/* Vi ønsker at købe en robot. Lav et program med 3 variabler (money, paycheck og robotCost). Sæt de 3 værdier til
+   henholdsvist 0 (vores nuværdene konto saldo), 5 (vores løn) og 100 (prisen på den seje robot). Forøg money med
+   paycheck (money += paycheck), så længe at (while-loop) money er mindre end robotCost. Når vi får råd til at købe
+   vores robot, så udskriv sætningen "Nu kan vi købe robotten!"*/
+?>;
