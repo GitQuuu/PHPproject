5ca3b8c6d4682e53ebf2f23449645626dd7ed80b
Qu <Quanvle@live.dk>
2019-05-10 11:55:14 +0200

    While loops i PHP
    2 part done

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
-    // udskriv 10-tabellen med en while loop
-    $Tabel = 1;
-    while ( $Tabel >= 10){
-        echo $Tabel;
+// udskriv 10-tabellen med en while loop
+
+
+echo "<br>";
+// udskriv tallen 0 til -100 med en while loop
+    $decrement = 0;
+    while ($decrement > -100){
+        echo $decrement;
         echo "<br>";
 
-        $Tabel++;
+        --$decrement;
     }
 
-// udskriv tallen 0 til 100 med en while loop
+    echo "$decrement";
 
 /* Vi ønsker at købe en robot. Lav et program med 3 variabler (money, paycheck og robotCost). Sæt de 3 værdier til
    henholdsvist 0 (vores nuværdene konto saldo), 5 (vores løn) og 100 (prisen på den seje robot). Forøg money med
    paycheck (money += paycheck), så længe at (while-loop) money er mindre end robotCost. Når vi får råd til at købe
    vores robot, så udskriv sætningen "Nu kan vi købe robotten!"*/
-?>;
+?>
