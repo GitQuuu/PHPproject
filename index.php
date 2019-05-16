e92cbdf0e58290a06e4406a1853e21734c8774b3
Qu <Quanvle@live.dk>
2019-05-16 08:43:05 +0200

    Date og time funktion i PHP 100%
    https://nemprogrammering.dk/Tutorials/PHP/PHP/20-date-time.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
-//Lav to simple HTML formularer. Lav et if-else statement, som enten printer den ene eller anden formular ud på
-// baggrund af en betingelse (boolsk udtryk), som du selv vælger. Test at det virker, ved at ændre i sandheden i
-// dit udtryk, således at du kan styre, hvilken formular der bliver vist.
-
-//Lav tre variabler med tekststrenge som værdier. Lave en HTML liste, hvor du via PHP indsætter værdien af dine
-// variabler som punkter i listen. Selve din HTML liste må selvfølgelig ikke skrives ud via HTML, det er kun værdierne
-// i variablerne der må det.
+//Udprint datoen i dag (uden klokkeslæt).
+echo date("d/m/Y");
+echo"<br>";
+//Udprint klokkeslæt i dag (uden dato).
+echo date("H:i:s");
+echo"<br>";
+//Udprint datoen, på formen (hvis det er søndag d. 20 juli): Sun, 20 juli
+echo date("D N M");
+echo "<br>";
+//Udprint datoen (på valgfri form) 2 uger i fremtiden.
+echo date("d/m/Y - H:i:s",(time()+(14*24*60*60)));
+echo "<br>";
+//Udprint datoen (på valgfri form) 1 dag tilbage i tiden, altså i går.
+echo date("d/m/Y - H:i:s",(time()-(1*24*60*60)));
+//*hint: Brug php.net under date funktionen for vejledning.
 ?>
